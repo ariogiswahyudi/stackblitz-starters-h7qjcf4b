d80ce0f69d9b3ac30a50c036436e4cf03261051f
ariogiswahyudi <ario.g.iswahyudi@gmail.com>
2025-10-16 15:24:08 +0700

    fourth

--- optional-04-oop/Inventory.js
+++ optional-04-oop/Inventory.js
@@ -8,7 +8,47 @@
  */
 
 class Inventory {
+    constructor() {
+        this.items = [];
+    }
+
+    addItem(item) {
+        if (item) {
+            this.items.push(item);
+            console.log(`Item dengan ID: ${this.id} berhasil ditambahkan!`);
+        } else {
+            console.error("Gagal menambahkan item: Item tidak valid");
+        }
+    }
+
+    removeItem(id) {
+        const initialLength = this.items.length;
+        this.items = this.items.filter(item => item.id !== id);
     
+        if (this.items.length < initialLength) {
+          console.log(`Item dengan ID ${id} berhasil dihapus.`);
+          return true;
+        } else {
+          console.warn(`Item dengan ID ${id} tidak ditemukan.`);
+          return false;
+        }
+    }
+
+    listItems() {
+        if (this.items.length === 0) {
+          return "Inventaris kosong.";
+        }
+    
+        const itemDetails = this.items.map(item => {
+          if (typeof item.displayDetails === 'function') {
+            return item.displayDetails(); // Memanggil method 'displayDetails' dari setiap item
+          } else {
+            return `Item ID ${item.id} tidak memiliki method displayDetails().`;
+          }
+        });
+    
+        return itemDetails.join('\n\n---\n\n'); // Menggabungkan detail item dengan pemisah
+    }
 }
 
 // Jangan hapus kode di bawah ini!
--- optional-04-oop/Item.js
+++ optional-04-oop/Item.js
@@ -9,7 +9,26 @@
  *   ```
  */
 
-class Item {}
+class Item {
+    constructor(id, name, quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    updateDetails(newName, newQuantity, newPrice) {
+        this.name = newName;
+        this.quantity = newQuantity;
+        this.price = newPrice;
+        console.log(`Detail item ID ${this.id} berhasil diperbarui!`);
+    }
+
+    displayDetails() {
+        return `ID: ${this.id}, Name: ${this.name}, Quantity: ${this.quantity}, Price: ${this.price}`;
+    }
+
+}
 
 // Jangan hapus kode di bawah ini!
 export default Item;
